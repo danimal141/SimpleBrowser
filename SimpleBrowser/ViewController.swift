a02eb63ddb6e1642d0664f90640884e083db72fe
danimal141 <hideaki.ishii1204@gmail.com>
2015-02-08 11:31:50 +0900

    Implement to display a sample webview

--- SimpleBrowser/ViewController.swift
+++ SimpleBrowser/ViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var backBtn: UIBarButtonItem!
+    @IBOutlet weak var forwardBtn: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let startUrl = "http://www.yahoo.co.jp/"
+        if let url = NSURL(string: startUrl) {
+            let urlRequest = NSURLRequest(URL: url)
+            self.webView.loadRequest(urlRequest)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func goBack(sender: AnyObject) {
+        self.webView.goBack()
+    }
+    @IBAction func goForward(sender: AnyObject) {
+        self.webView.goForward()
+    }
+    @IBAction func reload(sender: AnyObject) {
+        self.webView.reload()
+    }
 }
-
